6d276560bad7968b04f798a8c70096cc7f93b66f
Kartik Subramanian <kartik.subram@gmail.com>
2017-03-07 21:35:17 -0800

    Add index.js and update package.json to start a very simple server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send("We don't have tweets yet but we're working on it!")
+})
+
+app.listen(3000, function() {
+    console.log("Listening on 3000")
+})
+
