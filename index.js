61c0e793f3838d5b04161fd0d4c219b11bf58156
Kartik Subramanian <kartik.subram@gmail.com>
2017-03-20 22:26:14 -0700

    Add rabbit client (tortoise) and queue producer and consumer

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 const express = require('express')
 var Twitter = require('twitter')
+var Tortoise = require('tortoise')
 const app = express()
 
+tortoise = new Tortoise('amqp://localhost')
+
 var client = new Twitter({
-	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: ''
+
 })
 
-client.stream('statuses/filter', {follow: 'twitter'},  function(stream) {
-  stream.on('data', function(tweet) {
-    console.log(tweet.text);
-  });
+client.stream('statuses/filter', {follow: '25073877, 279118291'}, function(stream) {
+  stream.on('data', function(event) {
+  	if(event.user.id == 25073877 || event.user.id == 279118291) {
+  		//console.log(event.text)	
+  		tortoise
+    		.queue('twitter-stream', { durable: false })
+    		.publish({ event: event });
+  	} else {
+  		//console.log("Received unexpected event!")
+  	}
+    
+    //console.log(event && event.text);
 
-  stream.on('error', function(error) {
-    console.log(error);
   });
+ 
+  stream.on('error', function(error) {
+  	console.log(error)
+    throw error;
+  });
+});
+
+tortoise
+  .queue('twitter-stream', { durable: false })
+  .prefetch(1)
+  .subscribe(function(msg, ack, nack) {
+    // Handle 
+    console.log(msg)
+    var obj = JSON.parse(msg)
+    //console.log(obj)
+    //console.log(msg['event'])
+    console.log(obj.event.created_at)
+    dt = obj.event.created_at
+    db_date = new Date(Date.parse(dt))
+
+ //    pg_client.connect();
+ //    const query = pg_client.query
+ //    ('INSERT INTO tweets(json_text, created_at) VALUES($1, $2)', obj.event, db_date)
+	// query.on('end', () => { pg_client.end(); });
+
+
+	// pg.connect(connectionString, function (err, client, done) {
+	// 	if (err) {
+	// 	  // pass the error to the express error handler
+	// 	  return next(err)
+	// 	}
+	// 	client.query('INSERT INTO tweets(json_text, created_at) VALUES($1, $2)', [obj.event, db_date], function (err, result) {
+	// 	  done() //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
+
+	// 	  if (err) {
+	// 	    // pass the error to the express error handler
+	// 	    console.log(err)
+	// 	  }
+
+	// 	  //res.send(200)
+	// 	})
+	// })
+
+  ack(); // or nack(); 
 });
 
 app.get('/', (req, res) => {
