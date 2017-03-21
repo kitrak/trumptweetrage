fdfb09ce9d43a84dd9141a8d60b61df936ef2954
Kartik Subramanian <kartik.subram@gmail.com>
2017-03-20 21:57:52 -0700

    Add twitter

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express')
+var Twitter = require('twitter')
 const app = express()
+
+var client = new Twitter({
+	consumer_key: '',
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secret: ''
+})
+
+client.stream('statuses/filter', {follow: 'twitter'},  function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
 
 app.get('/', (req, res) => {
 	res.send("We don't have tweets yet but we're working on it!")
