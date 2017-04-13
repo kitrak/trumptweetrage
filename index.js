3a32f51a8c0592a82365fb7073c1fde2bdf67aec
Kartik Subramanian <kartik.subram@gmail.com>
2017-04-12 19:27:40 -0700

    Display last 3 tweets from database

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.engine('.hbs', exphbs({
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
 
+app.use(express.static('public'))
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))
 
@@ -130,8 +131,39 @@ tortoise
 });
 
 app.get('/', (req, res) => {
-	res.render('home', {
-    name: 'John'
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      //var tweet = db.collection('tweets').find().sort({created_at: -1}).limit(1);
+      db.collection('tweets').find().sort({created_at: -1}).limit(3).toArray(function(err, docs) {
+        
+        console.log("Printing doc from Array")
+        //console.log(docs[0].embed_html)
+
+        var twit = [docs[0].embed_html, docs[1].embed_html, docs[2].embed_html]
+
+        //console.log(twit)
+
+        res.render('home', {
+          tweets: twit
+        })  
+
+        // docs.forEach(function (doc) {
+        //   console.log("Doc from Array ");
+        //   //console.dir(doc);
+        //   var tweet = doc
+        //   console.log(tweet.embed_html)
+        //   // find the most recent tweet and display it
+        //   res.render('home', {
+        //     tweets: [tweet.embed_html]
+        //   })      
+        // })
+      })   
+    }
+    // res.render('home', {
+    //   tweet: 'Sorry, an error occurred'
+    // })
   })
 })
 
