b96384ca0b8c42f6d8554fffbfc66c9becf08eb1
Kartik Subramanian <kartik.subram@gmail.com>
2017-04-22 15:55:58 -0700

    Add twitter worker

---
+++ worker/twitter/twitter-worker.js
@@ -0,0 +1,22 @@
+const twitter = require('../../models/twitter')
+
+twitter.stream('statuses/filter', {follow: '25073877, 279118291'}, function(stream) {
+  stream.on('data', function(event) {
+  	if(event.user.id == 25073877 || event.user.id == 279118291) {
+  		console.log(event.text)	
+  		//tortoise
+    		//.queue('twitter-stream', { durable: false })
+    		//.publish({ event: event });
+  	} else {
+  		//console.log("Received unexpected event!")
+  	}
+    
+    //console.log(event && event.text);
+
+  });
+ 
+  stream.on('error', function(error) {
+  	console.log(error)
+    throw error;
+  });
+});
