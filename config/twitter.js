387fad55cf9f5875f486cc3ba7e5ed078c7f0649
Kartik Subramanian <kartik.subram@gmail.com>
2017-04-23 17:08:19 -0700

    Refactor stuff for twitter worker

--- config/twitter.js
+++ config/twitter.js
@@ -2,7 +2,7 @@
 
 //const common = require('./components/common')
 const logger = require('./components/logger')
-//const rabbitmq = require('./components/rabbitmq')
+const rabbitmq = require('./components/rabbitmq')
 const twitter = require('./components/twitter')
 
-module.exports = Object.assign({}, logger, twitter)
+module.exports = Object.assign({}, logger, twitter, rabbitmq)
--- worker/twitter/twitter-stream.js
+++ worker/twitter/twitter-stream.js
@@ -1,12 +1,14 @@
 const twitter = require('../../models/twitter')
+const tortoise = require('../../models/rabbitmq')
 
 twitter.stream('statuses/filter', {follow: '25073877, 279118291'}, function(stream) {
   stream.on('data', function(event) {
   	if(event.user.id == 25073877 || event.user.id == 279118291) {
+      //logger.info(event.text)
   		console.log(event.text)	
-  		//tortoise
-    		//.queue('twitter-stream', { durable: false })
-    		//.publish({ event: event });
+  		tortoise
+    		.queue('twitter-stream', { durable: false })
+    		.publish({ event: event });
   	} else {
   		//console.log("Received unexpected event!")
   	}
